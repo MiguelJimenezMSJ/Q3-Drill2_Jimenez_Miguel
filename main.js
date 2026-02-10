794a7cb20b1b225439db2767e07df7133d13bc96
MiguelJimenezMSJ <jimenez.ms@obmontessori.edu.ph>
2026-02-10 12:37:38 +0800

    Add files via upload

---
+++ main.js
@@ -0,0 +1,7 @@
+function computing_area( ) {
+    let base = Number(document.getElementById('base').value);
+    let height = Number(document.getElementById('height').value);
+    let area = (base * height)/2;
+
+    document.getElementById('output').innerHTML = 'The area of the triangle is ' + area;
+}
